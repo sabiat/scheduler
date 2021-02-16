0db892b1e75edbbfce2270872eaac349f8164156
Sabia Tahir <sabiatahir@hotmail.com>
2021-02-16 12:51:19 -0500

    Add style for hidden name

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "components/InterviewerListItem.scss";
+
+const classNames = require("classnames");
+
+
 
 export default function InterviewerListItem(props) {
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected 
+  })
+
+  const interviewerImageClass = classNames("interviewers__item-image", {
+    "interviewers__item-image--selected": props.selected 
+  })
+
+
   return (
-  <li className="interviewers__item">
+  <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
     <img
-      className="interviewers__item-image"
+      className={interviewerImageClass}
       src={props.avatar}
       alt={props.name}
     />
