3008ef6b7f3363410b40d5a09d72d1804cc5f2fc
Sabia Tahir <sabiatahir@hotmail.com>
2021-02-18 19:18:46 -0500

    Build custom hook for modes of Appointment component

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]); // keep track of history of modes
+
+  function transition(newMode, replace = false) { // add new mode to history
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, -1), newMode]);
+    } else {
+      setHistory([...history, newMode]);
+    }
+  }
+  function back() {
+    if (history.length === 1) return;
+    setHistory(prev => [...prev.slice(0, -1)]);
+  }
+  return { mode:history[history.length - 1], transition, back };
+}
+
+
+
+
