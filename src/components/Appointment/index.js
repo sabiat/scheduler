af818623acdbeca5cb4d05cebb958f6cbcabd707
Sabia Tahir <sabiatahir@hotmail.com>
2021-02-19 16:38:52 -0500

    Add mode to edit appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -18,7 +18,8 @@ export default function Appointment(props) {
   const CREATE = "CREATE";
   const SAVING = "SAVING";
   const DELETING = "DELETING";
-  const CONFIRM = "CONFIRM"
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -49,14 +50,24 @@ export default function Appointment(props) {
           student={props.interview.student} 
           interviewer={props.interview.interviewer}
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
         <Form
         interviewers={props.interviewers}
         onSave={save}
-        onCancel={() => back(EMPTY)}
+        onCancel={() => back()}
       />
+      )}
+      {mode === EDIT && (
+        <Form
+          name={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={() => back()}
+        />
       )}
       {mode === SAVING && (
         <Status message="Saving"/>
@@ -68,7 +79,7 @@ export default function Appointment(props) {
         <Confirm 
           message="Are you sure you would like to delete?"
           onConfirm={deleteAppt}
-          onCancel={() => back(EMPTY)}
+          onCancel={() => back()}
           />
       )}
     </article>
