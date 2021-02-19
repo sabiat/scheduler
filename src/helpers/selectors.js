3ed23f6ce345de086cd5e7f4f640031977b9af15
Sabia Tahir <sabiatahir@hotmail.com>
2021-02-18 21:25:48 -0500

    Add interviewers selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,6 @@
 export function getAppointmentsForDay(state, day) {
   const appointmentsInfoArray = [];
   const dayArray = state.days.filter(item => item.name === day);
-
   if (dayArray.length > 0) {
     const appointmentsArray = dayArray[0].appointments;
     for (const appointment of appointmentsArray) {
@@ -10,7 +9,23 @@ export function getAppointmentsForDay(state, day) {
   }
 
   return appointmentsInfoArray;
+
+
 }
+
+export function getInterviewersForDay(state, day) {
+  const interviewersInfoArray = [];
+  const dayArray = state.days.filter(item => item.name === day);
+
+  if (dayArray.length > 0) {
+    const interviewersArray = dayArray[0].interviewers;
+    for (const interviewer of interviewersArray) {
+      interviewersInfoArray.push(state.interviewers[interviewer]);
+    }
+  }
+    return interviewersInfoArray;
+  }
+
 
 export function getInterview(state, interview) {
   if (interview) {
